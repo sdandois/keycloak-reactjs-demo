5c37d922fa279c2332f63b054fcbd4a7b4e460e4
Santiago José Dandois <santiago.dandois@widergy.com>
2023-07-25 11:45:48 -0300

    ajustes

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -2,6 +2,8 @@ import Keycloak from "keycloak-js";
 
 const _kc = new Keycloak('/keycloak.json');
 
+
+
 /**
  * Initializes Keycloak instance and calls the provided callback function if successfully authenticated.
  *
@@ -9,20 +11,39 @@ const _kc = new Keycloak('/keycloak.json');
  */
 const initKeycloak = (onAuthenticatedCallback) => {
   _kc.init({
+
+    
     onLoad: 'check-sso',
+    // checkLoginIframe: false,
     silentCheckSsoRedirectUri: window.location.origin + '/silent-check-sso.html',
     pkceMethod: 'S256',
+    redirectUri: 'http://localhost:3000'
   })
     .then((authenticated) => {
       if (!authenticated) {
         console.log("user is not authenticated..!");
       }
       onAuthenticatedCallback();
+
+      console.log("ID Token: ", _kc.idTokenParsed)
+      console.log("Token: ", _kc.tokenParsed)
+
+
+      return fetch('http://localhost:8080/realms/widergy-directory/broker/oidc/token', {
+        headers: {
+          'Origin':'http://localhost:3000',
+          'Authorization': 'Bearer ' + _kc.token
+           
+        }
+      })
     })
+
     .catch(console.error);
 };
 
-const doLogin = _kc.login;
+const doLogin = () => _kc.login({ 
+  idpHint:'oidc'
+});
 
 const doLogout = _kc.logout;
 
@@ -41,6 +62,8 @@ const getUsername = () => _kc.tokenParsed?.preferred_username;
 
 const hasRole = (roles) => roles.some((role) => _kc.hasRealmRole(role));
 
+const getUserInfo = _kc.loadUserInfo
+
 const UserService = {
   initKeycloak,
   doLogin,
@@ -51,6 +74,7 @@ const UserService = {
   updateToken,
   getUsername,
   hasRole,
+  getUserInfo,
 };
 
 export default UserService;
